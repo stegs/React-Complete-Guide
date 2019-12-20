65f01fbba6cc892f805a7fbdae06160daa1869f0
Ryan Watson <ryan.watson@mlse.com>
2019-12-20 11:35:43 -0500

    60. Flexible Lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,13 +13,21 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-      this.setState({persons: [
-        { name: 'Ryan', age: 29 },
-        { name: event.target.value, age: 39 },
-        { name: 'Andy', age: 32 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -53,7 +61,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );
